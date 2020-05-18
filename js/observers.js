a4d07ed1aeaf6fd251ee74b8c2e1f1657771d7c1
Dimitar <dimitar_dimitrov12@mail.bg>
2020-05-18 12:57:13 +0300

    add video full screen

--- js/observers.js
+++ js/observers.js
@@ -20,3 +20,28 @@ const sectionOneObserver = new IntersectionObserver(function(
     sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+//! Aвтоматично скорлва до страницата
+
+$(document).ready(function() {
+    let docEl = $(document),
+        headerEl = $('header'),
+        headerWrapEl = $('.wrapHead'),
+        navEl = $('nav');
+
+
+    docEl.on('scroll', function() {
+
+        if (docEl.scrollTop() > 60) {
+            headerEl.addClass('fixed-to-top');
+            headerWrapEl.addClass('fixed-to-top');
+            navEl.addClass('fixed-to-top');
+        } else {
+            headerEl.removeClass('fixed-to-top');
+            headerWrapEl.removeClass('fixed-to-top');
+            navEl.removeClass('fixed-to-top');
+        }
+    });
+
+
+});
