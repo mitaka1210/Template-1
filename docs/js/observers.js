53a7eaf4c82b080e474be06febb6fdf8a88510c2
Dimitar <dimitar_dimitrov12@mail.bg>
2020-06-06 16:16:57 +0300

    add turkish and fix scroll top

--- docs/js/observers.js
+++ docs/js/observers.js
@@ -20,7 +20,6 @@ const sectionOneObserver = new IntersectionObserver(function(
     sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
-
 //! Aвтоматично скорлва до страницата
 function readywebsite() {
     
